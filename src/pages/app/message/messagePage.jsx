fdb32ece7b5c3d644b442f96abc6999be6d6d062
hasoro <noily4748@gmail.com>
2024-11-26 12:40:42 +0900

    letter fix 3

--- src/pages/app/message/messagePage.jsx
+++ src/pages/app/message/messagePage.jsx
@@ -1,7 +1,7 @@
 import MessageAside from "../../../components/app/message/messageAside";
 import MessageBox from "../../../components/app/message/messageBox";
 import AppLayout from "../../../layouts/app/AppLayout";
-import "../../../styles/App/message/message.scss";
+import "../../../styles/App/message/Message.scss";
 import "../../../styles/App/message/MessageAside.scss";
 
 export default function Message() {
