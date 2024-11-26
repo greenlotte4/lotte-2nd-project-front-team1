1e855efbd41347184fa478ed632d13e0b06c45ec
hasoro <noily4748@gmail.com>
2024-11-26 12:34:16 +0900

    latter fix

--- src/pages/app/project/ProjectPage.jsx
+++ src/pages/app/project/ProjectPage.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import Project from "../../../components/app/project/project";
+import React from "react";
+import Project from "../../../components/app/project/Project";
 import ProjectAside from "../../../components/app/project/ProjectAside";
 import AppLayout from "../../../layouts/app/AppLayout";
-import { useSidebar } from '../../../hooks/app/useSidebar'; // 커스텀 훅 사용
-import '../../../styles/App/project/Project.scss';
-import '../../../styles/App/AppAside.scss';
+import { useSidebar } from "../../../hooks/app/useSidebar"; // 커스텀 훅 사용
+import "../../../styles/App/project/Project.scss";
+import "../../../styles/App/AppAside.scss";
 
 export default function ProjectPage() {
-    const { isSidebarVisible, toggleSidebar } = useSidebar(); // 사이드바 상태 및 토글 함수 사용
+  const { isSidebarVisible, toggleSidebar } = useSidebar(); // 사이드바 상태 및 토글 함수 사용
 
-    return (
-        <AppLayout onToggleSidebar={toggleSidebar}>
-            {isSidebarVisible && <ProjectAside />}
-            <div className={`main-content ${isSidebarVisible ? '' : 'hidden-sidebar'}`}>
-                <Project />
-            </div>
-        </AppLayout>
-    );
+  return (
+    <AppLayout onToggleSidebar={toggleSidebar}>
+      {isSidebarVisible && <ProjectAside />}
+      <div
+        className={`main-content ${isSidebarVisible ? "" : "hidden-sidebar"}`}
+      >
+        <Project />
+      </div>
+    </AppLayout>
+  );
 }
