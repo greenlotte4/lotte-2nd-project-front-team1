c4f591a5c22878982e4ef1637bd8f961b65e27bb
Hasoro4748 <noily4748@gmail.com>
2024-11-27 02:08:11 +0900

    Update SubscriptionPage.jsx

--- src/pages/main/SubscriptionPage.jsx
+++ src/pages/main/SubscriptionPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Subscription from '../../components/main/Subscription';
 import '../../styles/Main/Include.scss';
-import '../../styles/main/Subscription.scss';
+import '../../styles/Main/Subscription.scss';
 import MainLayout from '../../layouts/main/MainLayout';
 
 const SubscriptionPage = () => {
