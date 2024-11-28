df089d5e9bc8b040cb416111e06269971567adb5
hasoro <noily4748@gmail.com>
2024-11-28 10:45:44 +0900

    letterfix1

--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import userSlice from "./slices/userSlice";
+import userSlice from "./slices/UserSlice";
 
 export default configureStore({
   reducer: {
