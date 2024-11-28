0ce8589e1273b0aa0d7dcf3298f941a4fe31c381
hasoro <noily4748@gmail.com>
2024-11-28 11:50:18 +0900

    letterfix5

--- src/layouts/app/AppLayout.jsx
+++ src/layouts/app/AppLayout.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import AppHeader from "../../components/app/include/AppHeader";
-import "../../styles/App/AppCommon.scss";
-import "../../styles/App/AppHeader.scss";
+import "../../styles/app/AppCommon.scss";
+import "../../styles/app/AppHeader.scss";
 // eslint-disable-next-line react/prop-types
 export default function AppLayout({ children, onToggleSidebar }) {
-    return (
-        <div id="container" className="Appcontainer">
-            {/* AppHeader에 사이드바 토글 함수를 prop으로 전달 */}
-            <AppHeader onToggleSidebar={onToggleSidebar} />
-            <div className="content-container">{children}</div>
-        </div>
-    );
+  return (
+    <div id="container" className="Appcontainer">
+      {/* AppHeader에 사이드바 토글 함수를 prop으로 전달 */}
+      <AppHeader onToggleSidebar={onToggleSidebar} />
+      <div className="content-container">{children}</div>
+    </div>
+  );
 }
