ce646736083288ce69d46c7b491a5b455fad85e6
dnjsrldus01 <dnjsrldus01@gmail.com>
2024-11-29 12:08:36 +0900

    board

---
+++ src/pages/app/board/MainBoardPage.jsx
@@ -0,0 +1,23 @@
+import BoardAside from "../../../components/app/board/BoardAside";
+import MainBoard from "../../../components/app/board/MainBoard";
+import { useSidebar } from "../../../hooks/app/UseSidebar";
+import AppLayout from "../../../layouts/app/AppLayout";
+
+import "../../../styles/app/board/Board.scss";
+import "../../../styles/app/board/BoardAside.scss";
+
+export default function Message() {
+    const { isSidebarVisible, toggleSidebar } = useSidebar(); // 사이드바 상태 및 토글 함수 사용
+  
+    return (
+      <AppLayout onToggleSidebar={toggleSidebar}>
+        <BoardAside isVisible={isSidebarVisible} />
+        <main
+          className={`main-content ${isSidebarVisible ? "" : "hidden-sidebar"}`}
+        >
+          <MainBoard />
+        </main>
+      </AppLayout>
+    );
+  }
+  
