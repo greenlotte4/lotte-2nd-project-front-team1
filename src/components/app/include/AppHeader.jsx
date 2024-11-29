9d29c203c949676cfca3567102750d025628d8cb
dnjsrldus01 <dnjsrldus01@gmail.com>
2024-11-29 12:51:34 +0900

    board1

--- src/components/app/include/AppHeader.jsx
+++ src/components/app/include/AppHeader.jsx
@@ -44,7 +44,7 @@ export default function AppHeader({ onToggleSidebar, noneAside }) {
         <Link to="/app/message" className="headerIcon">
           <img src="/images/messages.png" alt="message" />
         </Link>
-        <Link to="/app/article" className="headerIcon">
+        <Link to="/app/mainboard" className="headerIcon">
           <img src="/images/form.png" alt="article" />
         </Link>
         <Link to="/app/project" className="headerIcon">
