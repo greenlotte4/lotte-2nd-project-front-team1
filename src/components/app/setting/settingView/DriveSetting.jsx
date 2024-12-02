f06af1e693bebd2947a6a676cc8b9229f24a8c15
sujeonq <sujeonq@gmail.com>
2024-12-02 16:46:52 +0900

    update app-setting css

--- src/components/app/setting/settingView/DriveSetting.jsx
+++ src/components/app/setting/settingView/DriveSetting.jsx
@@ -1,49 +1,43 @@
 import { useState } from "react";
 
 export default function DriveSetting() {
-    const [driveNotifications, setDriveNotifications] = useState(() => {
-        const saved = localStorage.getItem("driveNotifications");
-        return saved ? JSON.parse(saved) : [
-            { id: 2, label: "경고 알림", checked: true },
-        ];
-    });
-    const handleNotificationChange = (id) => {
-        setDriveNotifications(prevNotifications =>
-            prevNotifications.map(notification =>
-                notification.id === id
-                    ? { ...notification, checked: !notification.checked }
-                    : notification
-            )
-        );
-        console.log('Checked state updated');
-    };
+  const [driveNotifications, setDriveNotifications] = useState(() => {
+    const saved = localStorage.getItem("driveNotifications");
+    return saved
+      ? JSON.parse(saved)
+      : [{ id: 2, label: "경고 알림", checked: true }];
+  });
+  const handleNotificationChange = (id) => {
+    setDriveNotifications((prevNotifications) =>
+      prevNotifications.map((notification) =>
+        notification.id === id
+          ? { ...notification, checked: !notification.checked }
+          : notification
+      )
+    );
+    console.log("Checked state updated");
+  };
 
+  return (
+    <div className="driveSetting">
+      <div className="switchSection">
+        <h5>사용량 경고 알림</h5>
 
-    return (
-        <div className="driveSetting">
-
-            <div className="switchSection">
-                <h4>사용량 경고 알림</h4>
-                <span>60% 이상시 경고 알림</span>
-                {driveNotifications.map((notification) => (
-                    <div key={notification.id} className="switch">
-                        <label className="switchLabel">
-                            <span className="notiName">{notification.label}</span>
-                            <input
-                                type="checkbox"
-                                checked={notification.checked}
-                                onChange={() => handleNotificationChange(notification.id)}
-                            />
-                            <span className="sliderBox round"></span>
-                        </label>
-                    </div>
-                ))}
-            </div>
-
-        
-
-
-
-        </div>
-    )
+        {driveNotifications.map((notification) => (
+          <div key={notification.id} className="switch">
+            <label className="switchLabel">
+              <span className="notiName">{notification.label}</span>
+              <input
+                type="checkbox"
+                checked={notification.checked}
+                onChange={() => handleNotificationChange(notification.id)}
+              />
+              <span className="sliderBox round"></span>
+            </label>
+            <span>60% 이상시 경고 알림</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
