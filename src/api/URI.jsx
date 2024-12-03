193a608492b9928bc8a44f96c3672525eef9082b
chldudwls <dudwls4251@gmail.com>
2024-12-03 15:39:57 +0900

    register

--- src/api/URI.jsx
+++ src/api/URI.jsx
@@ -1,9 +1,11 @@
 const SERVER_HOST = "http://localhost:8080";
 
 // user
-export const USER_URI = `${SERVER_HOST}/user`;
+export const USER_URI = `${SERVER_HOST}/user/register`;
 export const USER_LOGIN_URI = `${SERVER_HOST}/user/login`;
+export const USER_CHECK = `${SERVER_HOST}/user/`
 
+export const USER_EMAIL = `${SERVER_HOST}/api`
 // article
 
 // product
