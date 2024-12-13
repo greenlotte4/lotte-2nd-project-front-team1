034d6d66b928ede0eb27b0c57cb0dbf502795d30
chldudwls <dudwls4251@gmail.com>
2024-12-13 17:51:56 +0900

    update

--- src/socket.IO/NotificationSocket.jsx
+++ src/socket.IO/NotificationSocket.jsx
@@ -2,18 +2,16 @@
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 
-// 소켓 연결을 담당하는 컴포넌트
+// 알림을 처리하는 소켓 컴포넌트
 function NotificationSocket({ onAlertReceived }) {
   useEffect(() => {
-    // 현재 환경에 맞는 소켓 서버 주소 선택
-    const socketUrl = window.location.hostname === "localhost" 
-      ? "http://localhost:8080" 
+    const socketUrl = window.location.hostname === "localhost"
+      ? "http://localhost:8081"
       : "https://hubflow.store";
+    console.log("알림 소켓 연결");
 
-    // 소켓 인스턴스 생성
     const socket = io(socketUrl);
 
-    // 알림 수신 이벤트 리스너 등록
     const handleAlert = (data) => {
       console.log("알림 수신:", data);
       onAlertReceived(data); // 부모 컴포넌트에 알림 전달
@@ -21,14 +19,41 @@ function NotificationSocket({ onAlertReceived }) {
 
     socket.on("alert", handleAlert);
 
-    // 클린업: 소켓 연결 해제 및 이벤트 리스너 제거
+    // 클린업
     return () => {
       socket.off("alert", handleAlert);
       socket.disconnect();
     };
   }, [onAlertReceived]);
 
-  return null; // 이 컴포넌트는 UI를 렌더링하지 않음
+  return null; // UI를 렌더링하지 않음
 }
 
-export default NotificationSocket;
+// 메시지를 처리하는 소켓 컴포넌트
+function MessageSocket({ onMessageReceived }) {
+  useEffect(() => {
+    const socketUrl = window.location.hostname === "localhost"
+      ? "http://localhost:8081"
+      : "https://hubflow.store";
+    console.log("메시지 소켓 연결");
+
+    const socket = io(socketUrl);
+
+    const handleMessage = (data) => {
+      console.log("메시지 수신:", data);
+      onMessageReceived(data); // 부모 컴포넌트에 메시지 전달
+    };
+
+    socket.on("message", handleMessage);
+
+    // 클린업
+    return () => {
+      socket.off("message", handleMessage);
+      socket.disconnect();
+    };
+  }, [onMessageReceived]);
+
+  return null; // UI를 렌더링하지 않음
+}
+
+export { NotificationSocket, MessageSocket };
