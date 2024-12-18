e22d45b5403a16e7b88dd30851f8abadc63b2689
hasoro <noily4748@gmail.com>
2024-12-18 10:37:22 +0900

    messageWebSocket

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite"; // loadEnv 추가
 import react from "@vitejs/plugin-react";
+import nodePolyfills from "rollup-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
+  define: {
+    global: "window", // global을 window로 대체
+  },
 });
